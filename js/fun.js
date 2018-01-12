fab89dad00b8ff635a60a8f10b332e6e3972b38b
Chanel Rodriguez <13car92@gmail.com>
2018-01-12 16:56:24 -0600

    end of the day upload

---
+++ js/fun.js
@@ -0,0 +1,14 @@
+var a =3;
+var b =5;
+var c =15;
+var numtotal=[];
+var num3=[];
+var num5=[];
+var num15=[];
+for(var i = 0; i < 100; i++){
+    numtotal[i] = i;
+    num3[i] = i/a;
+    num5[i] = i/b;
+    num15[i] = i/c;
+}
+console.log(num3);
