4d4468e16000d0ec2d08f870466f758582ba30fc
Chanel Rodriguez <13car92@gmail.com>
2018-02-22 11:21:49 -0600

    promise files

--- js/promises.js
+++ js/promises.js
@@ -8,32 +8,32 @@ function wait(num){
     return new Promise((resolve,reject) => {
     setTimeout(() => {
         if (num !== 0) {
-            resolve();
+            resolve('some data');
         } else {
             reject('invalid milliseconds');
         }
     },num);
     });
 }
-
+//
 // wait(0).catch((data) => console.log(data));
-// wait(1000).then(() => console.log('You\'ll see this after 1 second'));
+// wait(1000).then((x) => console.log('You\'ll see this after 1 second ' + x));
 // wait(3000).then(() => console.log('You\'ll see this after 3 seconds'));
 
 
 
 
 
-// api: 86027bb23f8c3cc835df5579f3404e803cb4c525
 
-// fetch('https://api.github.com/users', {headers: {'Authorization': '86027bb23f8c3cc835df5579f3404e803cb4c525'}});
+
+// fetch('https://api.github.com/users', {headers: {'Authorization': 'TokenKey'}});
 
 
 function lastCommit(username){
     fetch(`https://api.github.com/users/${username}/events/public`, {headers:
-     {'Authorization': 'token 86027bb23f8c3cc835df5579f3404e803cb4c525' }}).then(response =>{
+     {'Authorization': 'token 53f1e1315324b557fc6c93380444de9c48925b0c' }}).then(response =>{
         response.json().then(data => {
-            console.log(data);
+            console.log(data[0][0]);
         })
     });
 }
